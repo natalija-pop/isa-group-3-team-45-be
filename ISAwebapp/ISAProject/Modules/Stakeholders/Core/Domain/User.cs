13a74750d05ead46ac8d480bf3b495c7fa15fdfd
AnjaMaksimovic <maksimovicanja11@gmail.com>
2023-11-15 17:27:50 +0100

    User model created.

--- ISAwebapp/ISAProject/Modules/Stakeholders/Core/Domain/User.cs
+++ ISAwebapp/ISAProject/Modules/Stakeholders/Core/Domain/User.cs
@@ -1,27 +1,46 @@
 ﻿using ISAProject.Configuration.Core.Domain;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion.Internal;
 
 namespace ISAProject.Modules.Stakeholders.Core.Domain
 {
     public class User : Entity
     {
-        public string Username { get; private set; }
+        public string Email { get; private set; }
         public string Password { get; private set; }
+        public string Name { get; private set; }
+        public string Surname { get; private set; }
+        public string City { get; private set; }
+        public string Country { get; private set; }
+        public string Phone { get; private set; }
+        public string Profession { get; private set; }
+        public string CompanyInformation { get; private set; }
         public UserRole Role { get; private set; }
-        public bool IsActive { get; set; }
 
-        public User(string username, string password, UserRole role, bool isActive)
+        public User(string email, string password, string name, string surname, string city, string country, string phone, string profession, string companyInformation, UserRole role)
         {
-            Username = username;
+            Email = email;
             Password = password;
+            Name = name;
+            Surname = surname;
+            City = city;
+            Country = country;
+            Phone = phone;
+            Profession = profession;
+            CompanyInformation = companyInformation;
             Role = role;
-            IsActive = isActive;
             Validate();
         }
-
         private void Validate()
         {
-            if (string.IsNullOrWhiteSpace(Username)) throw new ArgumentException("Invalid Name");
-            if (string.IsNullOrWhiteSpace(Password)) throw new ArgumentException("Invalid Surname");
+            if (string.IsNullOrWhiteSpace(Email)) throw new ArgumentException("Invalid Email");
+            if (string.IsNullOrWhiteSpace(Password)) throw new ArgumentException("Invalid Password");
+            if (string.IsNullOrWhiteSpace(Name)) throw new ArgumentException("Invalid Name");
+            if (string.IsNullOrWhiteSpace(Surname)) throw new ArgumentException("Invalid Surname");
+            if (string.IsNullOrWhiteSpace(City)) throw new ArgumentException("Invalid City");
+            if (string.IsNullOrWhiteSpace(Country)) throw new ArgumentException("Invalid Country");
+            if (string.IsNullOrWhiteSpace(Phone)) throw new ArgumentException("Invalid Phone");
+            if (string.IsNullOrWhiteSpace(Profession)) throw new ArgumentException("Invalid Profession");
+            if (string.IsNullOrWhiteSpace(CompanyInformation)) throw new ArgumentException("Invalid CompanyInformation");
         }
 
         public string GetPrimaryRoleName()
