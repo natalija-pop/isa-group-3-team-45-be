69cf3d7fc42da086cab778f7ee90c9107b7c4d9e
Natalija Popović <popovic.ra117.2020@uns.ac.rs>
2024-01-29 22:07:35 +0100

    test : Expanded test data

--- TestData/equipment.sql
+++ TestData/equipment.sql
@@ -2,4 +2,6 @@ INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "
 INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-2, 'Makaze', 'Hirurske makaze', 1, -1, 5, 0);
 INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-3, 'Gaza', 'Sterilna gaza', 2, -2, 5, 1);
 INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-4, 'Igla', 'Igla za zasivanje', 1, -2, 5, 1);
-INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-5, 'Stetoskop', 'Stetoskop rozi', 0, -3, 5, 1);
+INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-5, 'Stetoskop', 'Stetoskop rozi', 0, -3, 5, 0);
+INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-6, 'Stetoskop tip 2', 'Stetoskop plavi', 0, -4, 5, 0);
+INSERT INTO isa."Equipments"("Id", "Name", "Description", "Type", "CompanyId", "Quantity", "ReservedQuantity") VALUES (-7, 'Stetoskop tip 3', 'Stetoskop crveni', 0, -4, 5, 0);
